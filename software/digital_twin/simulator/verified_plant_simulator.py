a413b101eac39e611234b9673426f7cd0d174bd8
Thomas Wright <tom.tdw@gmail.com>
2022-04-12 12:31:30 +0000

    Initial integration of verified monitoring into self-adaption loop.

--- software/digital_twin/simulator/verified_plant_simulator.py
+++ software/digital_twin/simulator/verified_plant_simulator.py
@@ -1,30 +1,77 @@
-from typing import Tuple
+from typing import List, Tuple
 
 from sage.all import RIF
 import sage.all as sg
 import numpy as np
+from copy import deepcopy
+from concurrent.futures import ProcessPoolExecutor
 
 from verified_twin.incubator_models import SwitchingFourParameterModel
 from verified_twin.controllers import SignalArraySwitchedController
-from verified_twin.simulators import HybridSimulator
+from verified_twin.simulators import HybridSimulator, VerifiedContinuousSimulator
 from verified_twin.traces import VerifiedHybridTrace
+from verified_twin.incubator_models import fourpincubator
+from verified_twin.lbuc import Signal
 
 
-class VerifiedPlantSimulator4Params:
-    def run_simulation(self,
+class VerifiedPlantMonitor4Params:
+    def __init__(self, properties):
+        self.executor = ProcessPoolExecutor()
+        self.properties = properties
+
+    def verified_monitoring_results(self,
+            timespan_seconds, initial_box_temperature, initial_heat_temperature,
+            room_temperature, heater_on,
+            C_air, G_box, C_heater, G_heater) -> \
+            List[Signal]:
+        fut = self.executor.submit(
+            self.run_and_monitor,
+            self.properties,
+            timespan_seconds,
+            initial_box_temperature,
+            initial_heat_temperature,
+            room_temperature,
+            heater_on,
+            C_air,
+            G_box,
+            C_heater,
+            G_heater,
+        )
+        return fut.result()
+
+    @classmethod
+    def run_and_monitor(cls, properties,
+        timespan_seconds, initial_box_temperature,
+        initial_heat_temperature,
+        room_temperature, heater_on,
+        C_air, G_box, C_heater, G_heater) -> List[Signal]:
+        trace, model = cls.run_simulation(
+            timespan_seconds, initial_box_temperature,
+            initial_heat_temperature, room_temperature,
+            heater_on,
+            C_air, G_box, C_heater, G_heater,
+        )
+        monitoring_results = [
+            prop.signal(trace).G(-timespan_seconds[0])
+                for prop in properties
+        ]
+        return monitoring_results
+
+    @staticmethod
+    def run_simulation(
             timespan_seconds, initial_box_temperature, initial_heat_temperature,
             room_temperature, heater_on,
             C_air, G_box, C_heater, G_heater) -> \
             Tuple[VerifiedHybridTrace, SwitchingFourParameterModel]:
-
-        timetable = np.array(timespan_seconds)
+        timetable = np.array(deepcopy(timespan_seconds))
         # Need to feed room temp into box as signal: this will force a small timestep
         # but I will leave it for now for testing purposes!
-        heater_on_arr = np.array(heater_on)
+        heater_on_arr = np.array(deepcopy(heater_on))
 
-        model = SwitchingFourParameterModel([RIF(timespan_seconds[0]), initial_heat_temperature, initial_box_temperature],
-            T_R=room_temperature[0],
-            C_air=C_air, G_box=G_box, C_heater=C_heater, G_heater=G_heater)
+        model = SwitchingFourParameterModel([RIF(deepcopy(timespan_seconds[0])), deepcopy(initial_heat_temperature), deepcopy(initial_box_temperature)],
+             T_R=deepcopy(room_temperature[0]),
+             C_air=deepcopy(C_air), G_box=deepcopy(G_box),
+             C_heater=deepcopy(C_heater), G_heater=deepcopy(G_heater))
 
         controller = SignalArraySwitchedController(
             {'heater_on': False},
@@ -37,6 +84,13 @@ class VerifiedPlantSimulator4Params:
             controller_output_map=(lambda xin, x: xin),
         )
 
-        trace = simulator.run(time_limit=RIF(timespan_seconds[-1]) - RIF(timespan_seconds[0]))
+        import time
+        t1 = time.time()
+        trace = simulator.run(time_limit=(RIF(deepcopy(timespan_seconds[-1])) - RIF(deepcopy(timespan_seconds[0]))))
+        t2 = time.time()
+        print(f"ran verified simulation in {t2 - t1} sec")
+        # from time import sleep
+        # sleep(30)
 
-        return trace, model  # type: ignore
+        return trace, model # type: ignore
+        # return None, model # type: ignore
