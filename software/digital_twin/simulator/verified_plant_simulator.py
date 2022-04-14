8cce7650b1457f2881b59ca54c38a33f79216098
Thomas Wright <tom.tdw@gmail.com>
2022-04-14 23:22:37 +0000

    Apply optimized period open loop control strategy within the verification loop for predictive verified STL monitoring.

--- software/digital_twin/simulator/verified_plant_simulator.py
+++ software/digital_twin/simulator/verified_plant_simulator.py
@@ -1,3 +1,4 @@
+from curses.ascii import ctrl
 from typing import List, Tuple
 
 from sage.all import RIF
@@ -7,76 +8,74 @@ from copy import deepcopy
 from concurrent.futures import ProcessPoolExecutor
 
 from verified_twin.incubator_models import SwitchingFourParameterModel
-from verified_twin.controllers import SignalArraySwitchedController
+from verified_twin.controllers import PeriodicOpenLoopController, SignalArraySwitchedController
 from verified_twin.simulators import HybridSimulator, VerifiedContinuousSimulator
 from verified_twin.traces import VerifiedHybridTrace
 from verified_twin.incubator_models import fourpincubator
-from verified_twin.lbuc import Signal
+from verified_twin.lbuc import Signal, Logic
 
 
 class VerifiedPlantMonitor4Params:
-    def __init__(self, properties):
+    def __init__(self, properties: List[Logic]):
         self.executor = ProcessPoolExecutor()
         self.properties = properties
 
     def verified_monitoring_results(self,
-            timespan_seconds, initial_box_temperature, initial_heat_temperature,
-            room_temperature, heater_on,
+            tstart, tend, initial_box_temperature, initial_heat_temperature,
+            initial_room_temperature, ctrl_step_size, n_samples_period,
+            n_samples_heating,
             C_air, G_box, C_heater, G_heater) -> \
             List[Signal]:
+        prop_duration = max(prop.duration for prop in self.properties)
         fut = self.executor.submit(
             self.run_and_monitor,
             self.properties,
-            timespan_seconds,
-            initial_box_temperature,
-            initial_heat_temperature,
-            room_temperature,
-            heater_on,
-            C_air,
-            G_box,
-            C_heater,
-            G_heater,
+            tstart,
+            tend + prop_duration,
+            initial_box_temperature, initial_heat_temperature,
+            initial_room_temperature, ctrl_step_size, n_samples_period,
+            n_samples_heating,
+            C_air, G_box, C_heater, G_heater,
         )
         return fut.result()
 
     @classmethod
     def run_and_monitor(cls, properties,
-        timespan_seconds, initial_box_temperature,
-        initial_heat_temperature,
-        room_temperature, heater_on,
-        C_air, G_box, C_heater, G_heater) -> List[Signal]:
-        trace, model = cls.run_simulation(
-            timespan_seconds, initial_box_temperature,
-            initial_heat_temperature, room_temperature,
-            heater_on,
+        tstart, tend, initial_box_temperature, initial_heat_temperature,
+            initial_room_temperature, ctrl_step_size, n_samples_period,
+            n_samples_heating,
+            C_air, G_box, C_heater, G_heater) -> List[Signal]:
+        trace, _ = cls.run_simulation(
+            tstart, tend, initial_box_temperature, initial_heat_temperature,
+            initial_room_temperature, ctrl_step_size, n_samples_period,
+            n_samples_heating,
             C_air, G_box, C_heater, G_heater,
         )
         monitoring_results = [
-            prop.signal(trace).G(-timespan_seconds[0])
-                for prop in properties
+            prop.signal(trace).G(-tstart) for prop in properties
         ]
         return monitoring_results
 
     @staticmethod
     def run_simulation(
-            timespan_seconds, initial_box_temperature, initial_heat_temperature,
-            room_temperature, heater_on,
+            tstart, tend, initial_box_temperature, initial_heat_temperature,
+            initial_room_temperature, ctrl_step_size, n_samples_period,
+            n_samples_heating,
             C_air, G_box, C_heater, G_heater) -> \
             Tuple[VerifiedHybridTrace, SwitchingFourParameterModel]:
-        timetable = np.array(deepcopy(timespan_seconds))
         # Need to feed room temp into box as signal: this will force a small timestep
         # but I will leave it for now for testing purposes!
-        heater_on_arr = np.array(deepcopy(heater_on))
+        # Better yet, can we make this an interval?
 
-        model = SwitchingFourParameterModel([RIF(deepcopy(timespan_seconds[0])), deepcopy(initial_heat_temperature), deepcopy(initial_box_temperature)],
-             T_R=deepcopy(room_temperature[0]),
+        model = SwitchingFourParameterModel([RIF(deepcopy(tstart)), deepcopy(initial_heat_temperature), deepcopy(initial_box_temperature)],
+             T_R=deepcopy(initial_room_temperature),
              C_air=deepcopy(C_air), G_box=deepcopy(G_box),
              C_heater=deepcopy(C_heater), G_heater=deepcopy(G_heater))
 
-        controller = SignalArraySwitchedController(
-            {'heater_on': False},
-            timetable,
-            {'heater_on': heater_on_arr},
+        controller = PeriodicOpenLoopController(
+            ctrl_step_size,
+            n_samples_period,
+            n_samples_heating,
         )
 
         simulator = HybridSimulator(model, controller,
@@ -86,7 +85,7 @@ class VerifiedPlantMonitor4Params:
 
         import time
         t1 = time.time()
-        trace = simulator.run(time_limit=(RIF(deepcopy(timespan_seconds[-1])) - RIF(deepcopy(timespan_seconds[0]))))
+        trace = simulator.run(time_limit=(RIF(deepcopy(tend)) - RIF(deepcopy(tstart))))
         t2 = time.time()
         print(f"ran verified simulation in {t2 - t1} sec")
         # from time import sleep
